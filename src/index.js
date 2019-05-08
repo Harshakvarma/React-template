709e8a129f56a24e736b57e611bec1858c7d437e
harshakvarma <harsha6225@gmail.com>
2019-05-07 23:39:57 -0500

    added dashboard n landing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+
+import Paperbase from './modules/dashboard/Paperbase';
+import Index from './modules/landing/Home';
+import Privacy from './modules/landing/Privacy';
+import SignUp from './modules/landing/SignUp';
+
+
+const routing = (
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/dashboard" component={Paperbase} />
+        <Route path="/landing" component={Index} />
+        <Route path="/privacy" component={Privacy} />
+        <Route path="/signup" component={SignUp} />
+      </div>
+    </Router>
+  )
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
